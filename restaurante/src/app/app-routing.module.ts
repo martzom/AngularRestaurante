3db858e0abe7a3290eadcb3e1a307cde0498595f
Martzom <82251202+martzom@users.noreply.github.com>
2022-12-11 20:18:54 -0600

    se agrego carpetas en assets

--- restaurante/src/app/app-routing.module.ts
+++ restaurante/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { NavbaruserComponent } from './components/navbaruser/navbaruser.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
 {path: 'navbarus', component: NavbaruserComponent},
+{path: 'servicios', component: ServiciosComponent},
 {path:'**', redirectTo:'inicio', pathMatch:"full"},
 ];
 
--- restaurante/src/app/app.module.ts
+++ restaurante/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbaruserComponent } from './components/navbaruser/navbaruser.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbaruserComponent,
-    InicioComponent
+    InicioComponent,
+    ServiciosComponent,
+    AcercaComponent
   ],
   imports: [
     BrowserModule,
